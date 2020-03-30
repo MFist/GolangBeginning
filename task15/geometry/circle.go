11e2bdb5352f443a7deff8134ba2d9e69aedbdd4
MFist <36744810+MFist@users.noreply.github.com>
2020-03-30 18:13:01 -0500

    Tarea numero 15

---
+++ task15/geometry/circle.go
@@ -0,0 +1,16 @@
+package geometry
+
+import "math"
+
+//Circle ...
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perim() float64 {
+	return 2 * math.Pi * c.Radius
+}

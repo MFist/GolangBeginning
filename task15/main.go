3a96d3bc722caf35f9a0f85e2dfffad044547568
MFist <36744810+MFist@users.noreply.github.com>
2020-03-31 12:03:03 -0500

    Se hace na pequeña api con las figuras geometricas

--- task15/main.go
+++ task15/main.go
@@ -3,17 +3,54 @@ package main
 import (
 	"GolangBeginning/task15/geometry"
 	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
-func measure(g geometry.Geometry) {
-	fmt.Println(g)
-	fmt.Println(g.Area())
-	fmt.Println(g.Perim())
+var rect = geometry.Rect{Width: 3, Height: 4}
+var circle = geometry.Circle{Radius: 0}
+
+var err error
+var radius float64
+var width float64
+var height float64
+
+func areaHandler(c *gin.Context) {
+	if c.Param("radius") != "" {
+		radius, err = strconv.ParseFloat(c.Param("radius"), 64)
+		circle = geometry.Circle{Radius: radius}
+		c.JSON(200, gin.H{"Circle's area": geometry.Geometry.Area(circle)})
+	} else {
+		width, err = strconv.ParseFloat(c.Param("width"), 64)
+		height, err = strconv.ParseFloat(c.Param("height"), 64)
+		rect = geometry.Rect{Width: width, Height: height}
+		c.JSON(200, gin.H{"Rectangle's area": geometry.Geometry.Area(rect)})
+	}
+	fmt.Println(err)
+}
+
+func perimeterHandler(c *gin.Context) {
+	if c.Param("radius") != "" {
+		radius, err = strconv.ParseFloat(c.Param("radius"), 64)
+		circle = geometry.Circle{Radius: radius}
+		c.JSON(200, gin.H{"Circle's perimeter": geometry.Geometry.Perim(circle)})
+	} else {
+		width, err = strconv.ParseFloat(c.Param("width"), 64)
+		height, err = strconv.ParseFloat(c.Param("height"), 64)
+		rect = geometry.Rect{Width: width, Height: height}
+		c.JSON(200, gin.H{"Rectangle's perimeter": geometry.Geometry.Perim(rect)})
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func main() {
-	r := geometry.Rect{Width: 3, Height: 4}
-	c := geometry.Circle{Radius: 5}
-	measure(r)
-	measure(c)
+	r := gin.Default()
+	r.GET("/rectanglearea/:width/:height", areaHandler)
+	r.GET("/rectangleperimeter/:width/:height", perimeterHandler)
+	r.GET("/circlearea/:radius", areaHandler)
+	r.GET("/circleperimeter/:radius", perimeterHandler)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
